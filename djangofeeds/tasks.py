b6f5f315a7bfbf6edf38cf2ea368198175d51998
Ask Solem <askh@opera.com>
2009-07-29 20:05:13 +0200

    Disable even time distribution

--- djangofeeds/tasks.py
+++ djangofeeds/tasks.py
@@ -1,6 +1,5 @@
 from celery.task import tasks, Task, PeriodicTask, TaskSet
 from djangofeeds.importers import FeedImporter
-from djangofeeds.messaging import refresh_all_feeds_delayed
 from djangofeeds.models import Feed
 from django.conf import settings
 from django.core.cache import cache
@@ -102,7 +101,7 @@ class RefreshAllFeeds(PeriodicTask):
     """Periodic Task to refresh all the feeds.
 
     We evenly distribute the refreshing of feeds over the time
-    interval available.
+    interval available. (DISABLED)
 
     """
     run_every = REFRESH_EVERY
@@ -112,19 +111,30 @@ class RefreshAllFeeds(PeriodicTask):
         now = datetime.now()
         threshold = now - timedelta(seconds=REFRESH_EVERY)
         feeds = Feed.objects.filter(date_last_refresh__lt=threshold)
-        total = feeds.count()
-        if not total:
-            return
+
+        connection = DjangoAMQPConnection()
+        try:
+            for feed in feeds:
+                RefreshFeedTask.apply_async(connection=connection,
+                        kwargs={"feed_url": feed.feed_url,
+                                "feed_id": feed.pk})
+        finally:
+            connection.close()
+
+            
+        #total = feeds.count()
+        #if not total:
+        #    return
 
         # Time window is 75% of refresh interval in minutes.
-        time_window = REFRESH_EVERY * 0.75 / 60
+        #time_window = REFRESH_EVERY * 0.75 / 60
 
-        def iter_feed_task_args(iterable):
-            """For a feed in the db, return valid arguments for a
-            :class:`RefreshFeedTask`` task."""
-            for feed in iterable:
-                yield ([feed.feed_url], {}) # args,kwargs tuple
+        #def iter_feed_task_args(iterable):
+        #    """For a feed in the db, return valid arguments for a
+        #    :class:`RefreshFeedTask`` task."""
+        #    for feed in iterable:
+        #        yield ([feed.feed_url], {}) # args,kwargs tuple
         
-        it = iter_feed_task_args(feeds.iterator())
-        even_time_distribution(RefreshFeedTask, total, time_window, it) 
+        #it = iter_feed_task_args(feeds.iterator())
+        #even_time_distribution(RefreshFeedTask, total, time_window, it) 
 tasks.register(RefreshAllFeeds)
