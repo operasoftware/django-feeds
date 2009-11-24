92edd9cf28d8f46cfcd42ee3c3708643d5b49920
Ask Solem <askh@opera.com>
2009-11-24 13:57:38 +0100

    Added regression test for OPAL-578

--- djangofeeds/tests/test_importers.py
+++ djangofeeds/tests/test_importers.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import with_statement
+
 import os
+import sys
+import tempfile
 import unittest
+from contextlib import nested
+
+from django.contrib.auth import authenticate
+
+from yadayada.test.user import create_random_user
+
 from djangofeeds.importers import FeedImporter
 from djangofeeds.exceptions import FeedCriticalError
 from djangofeeds.exceptions import TimeoutError, FeedNotFoundError
 from djangofeeds.models import Feed, Post, Enclosure
-from yadayada.test.user import create_random_user
-from django.contrib.auth import authenticate
 
 data_path = os.path.join(os.path.dirname(__file__), 'data')
 
@@ -21,6 +28,28 @@ def get_data_filename(name):
 def get_data_file(name, mode="r"):
     with open(get_data_filename(name), mode) as file:
         return file.read()
+
+
+class TestRegressionOPAL578(unittest.TestCase):
+
+    def setUp(self):
+        self.importer = FeedImporter()
+        self.feeds = map(get_data_filename, ["t%d.xml" % i
+                                                for i in range(1, 10)])
+    def assertImportFeed(self, filename, name):
+        importer = self.importer
+        feed_obj = importer.import_feed(filename, local=True, force=True)
+        self.assertEqual(feed_obj.name, name)
+        return feed_obj
+
+    def test_does_not_duplicate_posts(self):
+        spool = tempfile.mktemp(suffix="ut", prefix="djangofeeds")
+        for i in range(20):
+            for filename in self.feeds:
+                with nested(open(filename), open(spool, "w")) as (r, w):
+                    w.write(r.read())
+                    f = self.assertImportFeed(spool, "Monsieur Le Chien")
+        self.assertEqual(f.post_set.all().count(), 10)
 
 
 class TestFeedImporter(unittest.TestCase):
