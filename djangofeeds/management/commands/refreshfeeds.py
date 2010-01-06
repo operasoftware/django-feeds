4056a06329d7cb746b7ba4bb55d5abab5bfef967
Ask Solem <askh@opera.com>
2010-01-06 14:14:17 +0100

    Refactor

--- djangofeeds/management/commands/refreshfeeds.py
+++ djangofeeds/management/commands/refreshfeeds.py
@@ -1,12 +1,27 @@
 from __future__ import with_statement
+
+import sys
+from optparse import make_option
+
 from yadayada.db import TransactionContext
 from yadayada.models import Language
-from django.core.management.base import CommandError, NoArgsCommand
+
 from django.conf import settings
 from django.core import exceptions
+from django.core.management.base import CommandError, NoArgsCommand
+
+from djangofeeds.tasks import refresh_feed
+from djangofeeds.models import Feed
 from djangofeeds.importers import refresh_all
-from djangofeeds.messaging import refresh_all_feeds_delayed
-from optparse import make_option
+
+
+def refresh_all_feeds_delayed(from_file=None):
+    urls = (feed.feed_url for feed in Feed.objects.all())
+    if from_file is not None:
+        with file(from_file) as feedfile:
+            urls = iter(feedfile.readlines())
+
+    map(refresh_feed.delay, urls)
 
 
 def refresh_all_feeds():
@@ -17,25 +32,21 @@ def refresh_all_feeds():
 class Command(NoArgsCommand):
     option_list = NoArgsCommand.option_list + (
         make_option('--lazy', '-l', action="store_true", dest="lazy",
-                    default=False, help="Delay the actual importing to AMQP"),
+                    default=False, help="Delay the actual importing to celery"),
         make_option('--file', '-f', action="store", dest="file",
                     help="Import all feeds from a file with feed URLs "
                     "seperated by newline."),
     )
 
-    help = ("Refresh feeds")
+    help = ("Refresh feeds", )
 
     requires_model_validation = True
     can_import_settings = True
 
     def handle_noargs(self, **options):
         lazy = options.get("lazy")
-        work = options.get("work")
         from_file = options.get("file")
-        concurrency = options.get("concurrency")
-        if from_file:
-            refresh_all_feeds_delayed(from_file=from_file)
-        elif lazy:
-            refresh_all_feeds_delayed()
+        if from_file or lazy:
+            refresh_all_feeds_delayed(from_file)
         else:
             refresh_all_feeds()
--- djangofeeds/tasks.py
+++ djangofeeds/tasks.py
@@ -5,7 +5,8 @@ from django.conf import settings
 from django.core.cache import cache
 
 from celery import conf as celeryconf
-from celery.task import Task, PeriodicTask
+from celery.utils import noop
+from celery.decorators import task
 
 from djangofeeds.models import Feed
 from djangofeeds.importers import FeedImporter
@@ -14,6 +15,7 @@ DEFAULT_REFRESH_EVERY = 3 * 60 * 60 # 3 hours
 DEFAULT_FEED_LOCK_CACHE_KEY_FMT = "djangofeeds.import_lock.%s"
 DEFAULT_FEED_LOCK_EXPIRE = 60 * 3 # lock expires in 3 minutes.
 
+ENABLE_LOCKS = False
 
 """
 .. data:: REFRESH_EVERY
@@ -63,7 +65,8 @@ FEED_LOCK_EXPIRE = getattr(settings,
                     DEFAULT_FEED_LOCK_EXPIRE)
 
 
-class RefreshFeedTask(Task):
+@task(routing_key="%s.feedimporter" % ROUTING_KEY_PREFIX, ignore_result=True)
+def refresh_feed(feed_url, feed_id=None, **kwargs):
     """Refresh a djangofeed feed, supports multiprocessing.
 
     :param feed_url: The URL of the feed to refresh.
@@ -71,81 +74,29 @@ class RefreshFeedTask(Task):
         the ``feed_url`` is used instead.
 
     """
-    routing_key = ".".join([ROUTING_KEY_PREFIX, "feedimporter"])
-    ignore_result = True
+    feed_id = feed_id or feed_url
+    lock_id = FEED_LOCK_CACHE_KEY_FMT % feed_id
 
-    def run(self, feed_url, feed_id=None, **kwargs):
-        feed_id = feed_id or feed_url
-        lock_id = FEED_LOCK_CACHE_KEY_FMT % feed_id
-
+    if ENABLE_LOCKS:
         is_locked = lambda: str(cache.get(lock_id)) == "true"
         acquire_lock = lambda: cache.set(lock_id, "true", FEED_LOCK_EXPIRE)
         release_lock = lambda: cache.set(lock_id, "nil", 1)
+    else:
+        acquire_lock = release_lock = noop
+        is_locked = lambda: False
 
-        logger = self.get_logger(**kwargs)
-        logger.info("Importing feed %s" % feed_url)
-        if is_locked():
-            logger.info("Feed is already being imported by another process.")
-            return feed_url
-
-        acquire_lock()
-        try:
-            importer = FeedImporter(update_on_import=True, logger=logger)
-            importer.import_feed(feed_url)
-        finally:
-            release_lock()
-
+    logger = refresh_feed.get_logger(**kwargs)
+    logger.info("Importing feed %s" % feed_url)
+    if is_locked():
+        logger.error("Feed is already being imported by another process.")
         return feed_url
 
 
-class RefreshFeedSlice(Task):
-    routing_key = ".".join([ROUTING_KEY_PREFIX, "slice"])
-    ignore_result = True
+    acquire_lock()
+    try:
+        importer = FeedImporter(update_on_import=True, logger=logger)
+        importer.import_feed(feed_url)
+    finally:
+        release_lock()
 
-    def run(self, start=None, stop=None, step=None, **kwargs):
-        feeds = get_feeds(start=start, stop=stop, step=None)
-
-        connection = self.establish_connection()
-        try:
-            for feed in feeds:
-                RefreshFeedTask.apply_async(connection=connection,
-                        kwargs={"feed_url": feed.feed_url,
-                                "feed_id": feed.pk})
-        finally:
-            connection.close()
-
-
-def get_feeds(start=None, stop=None, step=None):
-    threshold = datetime.now() - timedelta(seconds=REFRESH_EVERY)
-    feeds = Feed.objects.filter(date_last_refresh__lt=threshold)
-    if start or stop or step:
-        return feeds[slice(start, stop, step)]
-    return feeds
-
-
-class RefreshAllFeeds(PeriodicTask):
-    """Periodic Task to refresh all the feeds.
-
-    Splits the feeds into slices, depending how many feeds there are in
-    total and how many iterations you want it to run in.
-
-    :keyword iterations: The number of iterations you want the
-        work to complete in (default: 4).
-
-    """
-    routing_key = ".".join([ROUTING_KEY_PREFIX, "allrefresh"])
-    run_every = REFRESH_EVERY
-    ignore_result = True
-
-    def run(self, iterations=4, **kwargs):
-        logger = self.get_logger(**kwargs)
-        total = get_feeds().count()
-        win = REFRESH_EVERY * 0.80
-        size = ceil(win / iterations) * floor(total / win)
-        logger.info("TOTAL: %s WIN: %s SIZE: %s" % (total, win, size))
-
-        for i in xrange(iterations):
-            logger.info("APPLYING PAGE: %s (%s -> %s) c:%s" % (
-                i, i*size, (i+1)*size, ceil(win/ iterations)*i))
-            RefreshFeedSlice.apply_async(args=[(i*size, (i+1)*size)],
-                                   countdown=ceil(win / iterations)*i)
+    return feed_url
