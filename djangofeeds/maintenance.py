a672d6600d035d1d3b8016ea8aad56ed0da37080
Ask Solem <askh@opera.com>
2010-01-15 17:32:47 +0100

    Added the ability to control transaction commit frequency when expiring posts.

--- djangofeeds/maintenance.py
+++ djangofeeds/maintenance.py
@@ -1,9 +1,18 @@
-from djangofeeds.models import Feed, Post
-from datetime import datetime, timedelta
+from django.db import transaction
+
+from djangofeeds.models import Feed
 
 
-def expire_posts(min_posts=20):
-
+@transaction.commit_manually
+def expire_posts(min_posts=20, commit_every=10000):
+    total = 0
     for feed in Feed.objects.all():
         print("Expiring posts for %s" % feed.feed_url)
-        feed.expire_old_posts(min_posts=min_posts)
+        try:
+            deleted = feed.expire_old_posts(min_posts=min_posts)
+        except BaseException:
+            transaction.rollback()
+        else:
+            total += deleted
+            if not total % commit_every:
+                transaction.commit()
