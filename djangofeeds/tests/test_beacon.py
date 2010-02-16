2db6ad36ede486fc22a1d331058ad5f7f99308e2
Batiste Bieler <batisteb@opera.com>
2010-02-16 11:48:44 +0100

    100% coverage

--- djangofeeds/tests/test_beacon.py
+++ djangofeeds/tests/test_beacon.py
@@ -1,18 +1,19 @@
 import os
 import unittest
 from djangofeeds.optimization import BeaconRemover
+from django.conf import settings
+
+SERVICE_URLS = [
+    'http://a.rfihub.com/eus.gif?eui=2224',
+    'http://feeds.feedburner.com/~r/Wulffmorgenthaler/~4/aFiizovyBmA'
+]
 
 class TestBeacon(unittest.TestCase):
 
-    def test_urls(self):
+    def test_01_urls(self):
         br = BeaconRemover()
 
-        urls = [
-            'http://a.rfihub.com/eus.gif?eui=2224',
-            'http://feeds.feedburner.com/~r/Wulffmorgenthaler/~4/aFiizovyBmA'
-        ]
-
-        for url in urls:
+        for url in SERVICE_URLS:
             self.assertTrue(br.looks_like_beacon(url))
 
         urls = [
@@ -23,7 +24,7 @@ thaler?i=aFiizovyBmA:qUKuBzXJMEQ:V_sGLiPBpWU'
         for url in urls:
             self.assertFalse(br.looks_like_beacon(url))
 
-    def test_strip(self):
+    def test_02_strip(self):
         
         br = BeaconRemover()
         text = '''test %s %s'''
@@ -44,3 +45,20 @@ thaler?i=aFiizovyBmA:qUKuBzXJMEQ:V_sGLiPBpWU'
         parse_error = '<a< <img>'
         self.assertEqual(br.strip(parse_error), parse_error)
 
+        # special codition
+        condition = '<img src="">'
+        br.strip(condition)
+
+        
+
+    def test_03_settings(self):
+
+        from djangofeeds import optimization
+        setattr(optimization, 'DJANGOFEEDS_REMOVE_BEACON', False)
+        img = '<img src="%s" />'
+
+        br = BeaconRemover()
+        for url in SERVICE_URLS:
+            self.assertEqual(br.strip(img % url), img % url)
+
+        setattr(optimization, 'DJANGOFEEDS_REMOVE_BEACON', True)
