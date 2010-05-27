89048b8d72e0d9ffb7a677dfb608a2996670347a
Batiste Bieler <batisteb@opera.com>
2010-05-27 13:27:30 +0200

    Change the test that was not in fact testing enything.

--- djangofeeds/tests/test_importers.py
+++ djangofeeds/tests/test_importers.py
@@ -418,12 +418,12 @@ class TestFeedImporter(unittest.TestCase):
         self.assertTrue(len(feed["entries"]) > 0)
         for entry in feed["entries"]:
             guid = feedutil.get_entry_guid(feed, entry)
-            self.assertTrue(str(guid))
+            self.assertTrue(guid)
         
         imported_feed = self.importer.import_feed(feed_str, local=True, force=True)
         self.assertTrue(imported_feed.post_set.count() > 0)
         for post in imported_feed.post_set.all():
             guid = post.guid
-            self.assertTrue(str(guid))
+            self.assertTrue(guid)
 
         
