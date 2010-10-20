1e8161c99cfe7bdb6436480d6f98a3b6b4f88ea4
Batiste Bieler <batisteb@opera.com>
2010-10-20 16:27:46 +0200

    import_feed can return None

--- djangofeeds/tasks.py
+++ djangofeeds/tasks.py
@@ -43,7 +43,8 @@ def refresh_feed(feed_url, feed_id=None, importer_cls=None, **kwargs):
     try:
         importer = importer_cls(update_on_import=True, logger=logger)
         feed_obj = importer.import_feed(feed_url, force=True)
-        feed_obj.expire_old_posts()
+        if feed_obj is not None:
+            feed_obj.expire_old_posts()
     finally:
         release_lock()
 
